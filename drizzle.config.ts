f2262b47045bf5e5b2bfa41c9cf58b8935935085
Dale Cabarle <Danntabachoyy@gmail.com>
2026-05-11 09:46:32 +0800

    finally, migration completed

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from "drizzle-kit"
 
+import * as dotenv from "dotenv"
+dotenv.config()
+
+console.log(process.env.DATABASE_URL)
+
 export default defineConfig({
   schema: ["./lib/db/schema.ts", "./lib/db/auth-schema.ts"],
   out: "./lib/db/migrations",
